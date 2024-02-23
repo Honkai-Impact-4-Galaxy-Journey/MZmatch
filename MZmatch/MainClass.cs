300ca78087558f886fb620d8fbea0368a3c892f4
SilverWolf <atmlz32@163.com>
2024-02-23 23:45:00 +0800

    添加项目文件。

---
+++ MZmatch/MainClass.cs
@@ -0,0 +1,27 @@
+﻿using Exiled.API.Features;
+using Exiled.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MZmatch
+{
+    public class PluginConfig : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug {  get; set; } = false;
+    }
+    public class Plugin : Plugin<PluginConfig>
+    {
+        public override string Author => "Silver Wolf";
+        public override string Name => "MZmatch";
+        public override Version Version => new Version(1, 0, 0);
+        public static Plugin Instance { get; set; }
+        public override void OnEnabled()
+        {
+            Instance = this;
+        }
+    }
+}
